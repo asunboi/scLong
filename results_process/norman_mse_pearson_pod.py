5b7a3883bef1cf1330c8f112c7ab39f3637c1629
Ding Bai <86056326+BaiDing1234@users.noreply.github.com>
2025-02-05 11:14:44 +0800

    Update load yml

--- results_process/norman_mse_pearson_pod.py
+++ results_process/norman_mse_pearson_pod.py
@@ -3,71 +3,69 @@ from collections import defaultdict
 import numpy as np
 import pandas as pd
 from copy import deepcopy
-
 import matplotlib.pyplot as plt
-from brokenaxes import brokenaxes
 import yaml
 
-# 设置 matplotlib 使用 TrueType 字体 (Type 42) 而不是 Type 3
-plt.rcParams['pdf.fonttype'] = 42
-plt.rcParams['ps.fonttype'] = 42
 
-# 设置全局字体，可以选择其他常见的字体，比如 Arial
-plt.rcParams['font.family'] = 'DejaVu Sans'  # 或者 'Arial'
+
+
+# Set Arial globally in Matplotlib configuration
+plt.rcParams['font.family'] = 'DejaVu Sans'
+
+plt.rcParams['font.sans-serif'] = ['DejaVu Sans']
+plt.rcParams['font.size'] = 9
+plt.rcParams['svg.fonttype'] = 'none' 
+plt.rcParams['lines.linewidth'] = 0.5
 
 
 color_dict = {'GEARS': '#FFCC99',
               'Geneformer': '#99CC99',
-              'LongSC': '#CC99FF'}
+              'scGPT': '#FF9999',
+              'scFoundation': '#D1B26F',
+              'scLong': '#CC99FF'}
 
 with open('norman_mse_pod_pearson.yml', 'r', encoding='utf-8') as file:
     value_dict = yaml.load(file, Loader=yaml.FullLoader)
 
+z = np.random.normal()
 
 # Plotting     
-models = ['GEARS', 'Geneformer', 'LongSC']
+models = ['GEARS', 'Geneformer', 'scGPT', 'scFoundation', 'scLong']
 
-fig, axs = plt.subplots(1,3)
+fig, axs = plt.subplots(1,2)
 metric_to_plot = list(value_dict.keys())
 indices = np.arange(4)
-fig.set_size_inches(12.3, 3.3)
+fig.set_size_inches(7.2, 1.8)
 scenario_names = ['Seen 0/2', 'Seen 1/2', 'Seen 2/2', 'Seen 0/1']
 for i, metric in enumerate(metric_to_plot):
     ax = axs[i]
     width = 1 / (len(models) + 1)
     for model_idx, model in enumerate(models):
         values = np.array(value_dict[metric][model])[[0, 2, 4, 6]]
-        if i == 1:
-            values = 1-values
         errors = np.array(value_dict[metric][model])[[1, 3, 5, 7]]
         label = model
         ax.bar(indices + model_idx * width, values, yerr=errors, label=label, width=width, color= color_dict[model]) #, color=colors[model_idx % len(colors)])
-    
+    if i == 0:
+        ax.set_ylim(0.1,0.26)
     if i == 1:
-        ax.set_ylim(0,0.15)
-    elif i == 2:
-        ax.set_ylim(0,1)
+        ax.set_ylim(0.5,0.92)
 
-    ax.set_xlabel('Scenario', fontsize = 13)
-    ax.set_ylabel(f'{metric}', fontsize = 13)
-    #plt.title(f'Histogram of {column} by Model')
-    ax.set_xticks(ticks=indices + width * (len(models) - 1) /2)
-    ax.set_xticklabels(scenario_names, fontsize=11)
+    #ax.set_xlabel('Scenario', fontsize = 11.5)
+    ax.set_ylabel(f'{metric}')
+    if i == 0:
+        ax.set_yticks(ticks = np.arange(2, 6) * 0.05)
     if i == 1:
-        ax.legend(loc = 'upper left', fontsize=8.5,frameon=False)
-    else:
-        ax.legend(loc = 'upper right', fontsize=8.5,frameon=False)
+        ax.set_yticks(ticks = np.arange(5, 10) * 0.1)
+    ax.set_xticks(ticks=indices + width * (len(models) - 1) /2)
+    ax.set_xticklabels(scenario_names)
     
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
 
 fig.subplots_adjust(hspace=0, wspace=0.1)
 
-
-plt.title('Genetic perturbtion effects prediction')
-
 fig.tight_layout()
-fig.savefig(f'figs/norman_split_1_mse_pod_pearson.pdf', dpi = 300)
+fig.savefig(f'figs/norman_split_1_mse_pearson.svg', format='svg')
 fig = None
 plt.close()
 
@@ -93,5 +91,5 @@ for i, metric in enumerate(metric_to_plot):
     #ax.spines['top'].set_visible(False)
 
     fig.tight_layout()
-    fig.savefig(f'figs/norman_split_1_pearson.pdf', dpi = 300)
+    fig.savefig(f'figs/norman_split_1_pearson.svg', dpi = 300)
     fig = None'''
